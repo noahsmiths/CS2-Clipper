cab6e3dd7705c6b1edef2c8b9fc568c535645f89
Noah Smith <nsmith2@unc.edu>
2024-09-29 20:48:12 -0400

    Add configuration options

--- packages/renderer-client/index.ts
+++ packages/renderer-client/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello via Bun!");
+const CONFIG_FILE_PATH = "./config.json";
+const configFile = Bun.file(CONFIG_FILE_PATH);
+
+if (await configFile.exists() == false) {
+    console.error("Could not find a config.json file. Make sure to copy the template one from the repository and place it in the same directory as this executable.");
+    process.exit(1);
+}
+
+const config = await configFile.json();
