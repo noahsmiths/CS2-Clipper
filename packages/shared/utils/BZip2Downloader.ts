2c0d5d1e658854adfea5e94b59a245986a64a9a7
Noah Smith <nsmith2@unc.edu>
2025-01-27 03:23:04 -0500

    Fix bz2 downloader to handle decompression errors

--- packages/shared/utils/BZip2Downloader.ts
+++ packages/shared/utils/BZip2Downloader.ts
@@ -6,11 +6,13 @@ export function downloadBZip2(url: string, destinationPath: string): Promise<voi
     return new Promise((resolve, reject) => {
         http.get(url, (response) => {
             const writeStream = fs.createWriteStream(destinationPath, { flags: "w", autoClose: true, flush: true });
+            const bz2Stream = bz2();
 
-            response.pipe(bz2()).pipe(writeStream);
+            response.pipe(bz2Stream).pipe(writeStream);
 
             writeStream.once("close", resolve); // Must close on writestream, not response, or else some bytes can get lost
             response.once("error", reject);
+            bz2Stream.once("error", reject);
         })
         .once("error", reject);
     });
